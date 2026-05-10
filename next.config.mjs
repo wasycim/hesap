8543988713a8ea9fdbd5ce77b08bbf2e725112f1
wasycim <quixewashere@gmail.com>
2026-05-10 16:35:04 +0300

    Disable Vercel toolbar header

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "x-vercel-skip-toolbar",
+            value: "1",
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
